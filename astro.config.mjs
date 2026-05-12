8f52c934b277913c76b9d23869d390f126e73703
rcampbell30 <camperbell19@gmail.com>
2026-05-12 03:59:43 +0100

    Configure Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://rcampbell30.github.io',
+  base: '/Vigil',
   integrations: [tailwind()],
   output: 'static',
 });
